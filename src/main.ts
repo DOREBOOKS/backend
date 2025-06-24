9337496332d727a8e1f7f1c2da17831e8d710816
sj0919 <seungjinlee09@gmail.com>
2025-06-24 18:08:14 +0900

    ♻️ refactor : main.ts 수정

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import 'reflect-metadata';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  //정적 파일 서빙:/uploads 경로에 접근 가능하도록 설정
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/', // 예: http://localhost:3000/uploads/books/filename.jpg
+  });
+
   // global Validation pipe
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
@@ -14,11 +21,11 @@ async function bootstrap() {
     .setTitle('My API')
     .setDescription('API 문서 예시')
     .setVersion('1.0')
-    .addTag('users')  // Add tags for each endpoint as needed
+    .addTag('users') // Add tags for each endpoint as needed
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  
+
   SwaggerModule.setup('api-docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
