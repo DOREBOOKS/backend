7bfc2604bb8fbfa434ad8ebb238f2e42bbb71566
sj0919 <seungjinlee09@gmail.com>
2025-09-08 14:00:28 +0900

    ♻️refactor : users module에 DealsModule 추가

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,16 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
 import { UsersController } from './controller/users.controller';
 import { UsersService } from './service/users.service';
 import { DealsEntity } from 'src/deal/entity/deals.entity';
+import { DealsModule } from 'src/deal/deals.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity, DealsEntity])],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity, DealsEntity]),
+    forwardRef(() => DealsModule),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [TypeOrmModule, UsersService],
