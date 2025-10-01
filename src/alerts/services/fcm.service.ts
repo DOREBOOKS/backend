62a76489ec0ac644d3faa9d0d39f5d1303f5cfd6
qwerq123456 <qwerq123456@snu.ac.kr>
2025-10-01 17:54:16 +0900

    🐛 fix: env object error

--- src/alerts/services/fcm.service.ts
+++ src/alerts/services/fcm.service.ts
@@ -11,17 +11,33 @@ export class FcmService {
 
   constructor() {
     //const keyFile = process.env.GOOGLE_APPLICATION_CREDENTIALS;
-    const keyJson = process.env.FIREBASE_SERVICE_ACCOUNT_JSON;
-
-    let key: any;
-    if (keyJson) {
-      key = JSON.parse(keyJson);
-      // } else if (keyFile) {
-      //   key = JSON.parse(fs.readFileSync(keyFile, 'utf8'));
-      // }
-    } else {
-      throw new Error('No Firebase credentials provided');
-    }
+    // const keyJson = process.env.FIREBASE_SERVICE_ACCOUNT_JSON;
+    const key = {
+      type: process.env.FIREBASE_SERVICE_ACCOUNT_TYPE as string,
+      project_id: process.env.FIREBASE_SERVICE_ACCOUNT_PROJECT_ID as string,
+      private_key_id: process.env
+        .FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY_ID as string,
+      private_key: process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY as string,
+      client_email: process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_EMAIL as string,
+      client_id: process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_ID as string,
+      auth_uri: process.env.FIREBASE_SERVICE_ACCOUNT_AUTH_URI as string,
+      token_uri: process.env.FIREBASE_SERVICE_ACCOUNT_TOKEN_URI as string,
+      auth_provider_x509_cert_url: process.env
+        .FIREBASE_SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL as string,
+      client_x509_cert_url: process.env
+        .FIREBASE_SERVICE_ACCOUNT_CLIENT_X509_CERT_URL as string,
+      universe_domain: process.env
+        .FIREBASE_SERVICE_ACCOUNT_UNIVERSE_DOMAIN as string,
+    };
+    // let key: any;
+    // if (keyJson) {
+    //   key = JSON.parse(keyJson);
+    //   // } else if (keyFile) {
+    //   //   key = JSON.parse(fs.readFileSync(keyFile, 'utf8'));
+    //   // }
+    // } else {
+    //   throw new Error('No Firebase credentials provided');
+    // }
 
     this.projectId = key.project_id;
     this.auth = new GoogleAuth({
