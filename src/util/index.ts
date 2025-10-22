d4c03f5b84570c33587a343175b74885262f87a5
qwerq123456 <qwerq123456@snu.ac.kr>
2025-10-23 01:31:10 +0900

    fix : env 띄어쓰기 누락 수동 수정

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,13 +1,15 @@
 function normalizePrivateKey(raw?: string) {
   if (!raw) return raw;
-  console.log('Normalizing private key');
-  console.log('raw: ', raw.slice(0, 30) + '...'); // 로그에 전체 키가 노출되지 않도록 일부만 출력
+  // console.log('Normalizing private key');
+  // console.log('raw: ', raw.slice(0, 30) + '...'); // 로그에 전체 키가 노출되지 않도록 일부만 출력
   let key = raw.trim();
   key = key.replace(/\\r/g, '');
   key = key.replace(/^"(.*)"$/, '$1').replace(/^'(.*)'$/, '$1'); // 양끝 따옴표 방어적 제거
   if (key.includes('\\n') && !key.includes('\n')) {
     key = key.replace(/\\n/g, '\n'); // \n => 진짜 줄바꿈
   }
+  key.replace('----BEGINPRIVATEKEY----', '-----BEGIN PRIVATE KEY-----');
+  key.replace('----ENDPRIVATEKEY----', '-----END PRIVATE KEY-----');
   return key;
 }
 
