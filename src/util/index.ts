783f58df9bce843e616f7e5a70cf93bbf1f4dd49
qwerq123456 <qwerq123456@snu.ac.kr>
2025-10-23 01:22:15 +0900

    fix : env 공백 제거 기능 제거

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,7 +1,7 @@
 function normalizePrivateKey(raw?: string) {
   if (!raw) return raw;
   let key = raw.trim();
-  key = key.replace(/\\r/g, '');
+  // key = key.replace(/\\r/g, '');
   key = key.replace(/^"(.*)"$/, '$1').replace(/^'(.*)'$/, '$1'); // 양끝 따옴표 방어적 제거
   if (key.includes('\\n') && !key.includes('\n')) {
     key = key.replace(/\\n/g, '\n'); // \n => 진짜 줄바꿈
